57f23931ed27ee363216ae07cd92dfb979f75cf6
eknerph <eknerph@gmail.com>
2024-10-17 22:04:37 -0300

    adicionado códigos para alteração de Meu currículo para Meu Portifólio com modificações

--- app.rb
+++ app.rb
@@ -8,36 +8,44 @@ set :public_folder, 'public'
 get '/' do
     <<-HTML
     <link rel="stylesheet"  type="text/css" href="/styles.css">
-    <h1>Meu Portifólio</h1>
-    <ul>
-        <li><a href="/experiencia">Experiência</a></li>
-        <li><a href="/educacao">Educação</a></li>
-        <li><a href="/habilidades">Habilidades</a></li>
-        <li><a href="/contato">Contato</a></li>
-    </ul>
+    <div class="container">
+        <h1>Meu Portifólio</h1>
+        <ul>
+            <li><a href="/experiencia">Experiência</a></li>
+            <li><a href="/educacao">Educação</a></li>
+            <li><a href="/habilidades">Habilidades</a></li>
+            <li><a href="/contato">Contato</a></li>
+        </ul>
+    </div>
     HTML
 end
 
 # Seçao de Experiência
 get '/experiencia' do
     <<-HTML
-    <h2>Experiência Profissional</h2>
-    <ul>
-        <li>Desenvolvedor na Empresa X (2001 - Presente)</li>
-        <li>Estágio na Empresa Y (2019 - 2021)</li>
-    </ul>
-    <a href="/">Voltar</a>
+    <link rel="stylesheet" type="text/css" href="/styles.css">
+    <div class="conteiner">
+        <h2>Experiência Profissional</h2>
+        <ul>
+            <li>Desenvolvedor na Empresa X (2001 - Presente)</li>
+            <li>Estágio na Empresa Y (2019 - 2021)</li>
+        </ul>
+        <a href="/" class="voltar">Voltar</a>
+    </div>
     HTML
 end
 
 # Seção de Educação
 get '/educacao' do
     <<-HTML
-    <h2>Educação</h2>
-    <ul>
-        <li>Bacharelado em Ciência da Computação - Univercidade Cruzeiro do Sul (2023 - 2025)</li>
-    </ul>
-    <a href="/">Voltar</a>
+        <link rel="stylessheet" type="text/css" href="/styles.css">
+        <div class="container">
+        <h2>Educação</h2>
+        <ul>
+            <li>Bacharelado em Ciência da Computação - Univercidade Cruzeiro do Sul (2023 - 2025)</li>
+        </ul>
+        <a href="/" class="voltar">Voltar</a>
+    </div>
     HTML
 end
 
