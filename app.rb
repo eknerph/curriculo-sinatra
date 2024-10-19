e9187335168927d7a8a84f141a28bb7826576409
eknerph <eknerph@gmail.com>
2024-10-19 16:42:04 -0300

    Adicionado arquivos e instala a gem os e bundle

--- app.rb
+++ app.rb
@@ -1,30 +1,22 @@
 # app.rb
 require 'sinatra'
 
-# Habilitando o uso de arquivos estáticos (CSS) ++
+# Define a pasta pública para arquivos CSS e imagens
 set :public_folder, 'public'
 
-# Página inicial
+# Página inicial currículo
 get '/' do
     <<-HTML
     <link rel="stylesheet"  type="text/css" href="/styles.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+
     <div class="header">
         <div class="profile-container">
             <div class="profile-photo"
-                <img src="https://via.placeholder.com/300" alt="Foto do perfil">
+                <img src="https://sl.bing.net/flFeMyRgcqy" alt="Foto do perfil">
             </div>
-            <div class="profile-info">            
-                <h1>Ekner Alexandre de Almeida</h1>
-                <h2>Desenvolvedor</h2>
-                <p><strong>LinkedIn:</strong> www.linkedin.com/in/eknereaa</p>
-                <p><strong>Github:</strong> https://github.com/eknerph</p>
-                <p><strong>E-mail:</strong> eknerph@gmail.com</p>
-
-                <div class="social-links">
-                    <a href="#"><i class="fab fa-linkedIn"></i></a>
-
             
-        </div>
     HTML
 end
 
