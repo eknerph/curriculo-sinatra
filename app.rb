d719f80ac31963c3e627d44c8d224b79abc0114d
eknerph <eknerph@gmail.com>
2024-10-21 19:42:45 -0300

    Adicionados arquivos de imagem da foto do portifólio alterado itens do arquivo styles.css, adição  da pasta viewa/index.erb e adicionado itens no app.rb

--- app.rb
+++ app.rb
@@ -2,21 +2,23 @@
 require 'sinatra'
 
 # Define a pasta pública para arquivos CSS e imagens
-set :public_folder, 'public'
+set :public_folder, File.dirname(__FILE__) + '/public'
+
 
 # Página inicial currículo
 get '/' do
+
     <<-HTML
     <link rel="stylesheet"  type="text/css" href="/styles.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
     <div class="header">
         <div class="profile-container">
-            <div class="profile-photo"
-                <img src="https://sl.bing.net/flFeMyRgcqy" alt="Foto do perfil">
+            <div class="profile-photo">
+                <img src="/ekner_picture.png" alt="Foto do perfil" />
             </div>
             <div class="profile-info">
-                <h1>Ekner Alexandre de ALmeida</h1>
+                <h1>Ekner Alexandre de Almeida</h1>
                 <h2>Desenvolvedor</h2>
                 <p><strong>LinkedIn:</strong> www.linkedin.com/in/eknereaa</p>
                 <p><strong>Github:</strong> https://github.com/eknerph</p>
@@ -24,10 +26,10 @@ get '/' do
                 <p><strong>E-mail:</strong> eknerph@gmail.com</p>
 
                 <div class="social-links">
-                    <a href="#"><1 class="fab fa-linkedin"></i></a>
-                    <a href="#"><1 class="fab fa-facebook"></i></a>
-                    <a href="#"><1 class="fab fa-twitter"></i></a>
-                    <a href="#"><1 class="fab fa-github"></i></a>
+                    <a href="#"><i class="fab fa-linkedIn"></i></a>
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-github"></i></a>
                 </div>
             </div>        
         </div> 
@@ -35,60 +37,3 @@ get '/' do
     HTML
 end
 
-# # Seçao de Experiência
-# get '/experiencia' do
-#     <<-HTML
-#     <link rel="stylesheet" type="text/css" href="/styles.css">
-#     <div class="container">
-#         <h2>Experiência Profissional</h2>
-#         <ul>
-#             <li>Desenvolvedor na Empresa X (2001 - Presente)</li>
-#             <li>Estágio na Empresa Y (2019 - 2021)</li>
-#         </ul>
-#         <a href="/" class="voltar">Voltar</a>
-#     </div>
-#     HTML
-# end
-
-# # Seção de Educação
-# get '/educacao' do
-#     <<-HTML
-#     <link rel="stylesheet" type="text/css" href="/styles.css">
-#     <div class="container">
-#         <h2>Educação</h2>
-#         <ul>
-#             <li>Bacharelado em Ciência da Computação - Univercidade Cruzeiro do Sul (2023 - 2025)</li>
-#         </ul>
-#         <a href="/" class="voltar">Voltar</a>
-#     </div>
-#     HTML
-# end
-
-# # Seção de Habilidades
-# get '/habilidades' do
-#     <<-HTML
-#     <link rel="stylesheet" type="text/css" href="/styles.css">
-#     <div class="container">
-#         <h2>Habilidades</h2>
-#         <ul>
-#             <li>Ruby, Sinatra, HTML, CSS</li>
-#             <li>Banco de Dados (PostgreSQL, MySQL)</li>
-#             <li>Git e Controle de Versão</li>
-#         </ul>
-#         <a href="/" class="voltar">Voltar</a>
-#     </div>
-#     HTML
-# end
-
-# # Seção Contato
-# get '/contato' do
-#     <<-HTML
-#     <link rel="stylesheet" type="text/css" href="/styles.css">
-#     <div class="container">
-#         <h2>Contato</h2>
-#         <p>Email: eknerph@gmail.com</p>
-#         <p>LinkedIn: <a href="https://linkedin.com/in/eknereaa" target="_blank">Meu Perfil</a></p> 
-#         <a href="/" class="voltar">Voltar</a>
-#     </div>
-#     HTML
-# end
