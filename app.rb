adbf928317108ded3ba7cc19ac07b18712b24209
eknerph <eknerph@gmail.com>
2024-10-30 22:59:25 -0300

    Alterando e fazendo ajustes nos códigos app.rb e public/styles

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@
 require 'sinatra'
 
 # Define a pasta pública para arquivos CSS e imagens
-set :public_folder, File.dirname(__FILE__) + '/public'
+# set :public_folder, File.dirname(__FILE__) + '/public'
 
 
 # Página inicial currículo
